c7d1c937a380c9b21ecb4770317267b64f3269a2
Gaku52 <Gaku52@example.com>
2022-10-18 03:47:17 +0900

    feat: 問題個所の特定

--- src/components/features/story/Stories/Story.tsx
+++ src/components/features/story/Stories/Story.tsx
@@ -21,7 +21,7 @@ export const Story = ({ title, coverImage, date, excerpt, slug }: Props) => {
             className="w-full max-w-xs h-full object-cover"
           />
         </div>
-        <div className="md:w-2/3 p-2 md:p-6 vstack gap-2 bg-primary-3">
+        <div className="md:w-2/3 p-4 md:p-6 vstack gap-2 bg-primary-1">
           <Date date={date} />
           <h3 className="text-primary-1 text-xl font-medium">{title}</h3>
           <p className="text-neutral-700 dark:text-neutral-300 font-normal text-md line-clamp-2 md:line-clamp-3">

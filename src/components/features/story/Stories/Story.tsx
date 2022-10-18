b2d17b1af070dc8e4b1c508ddca0b50b48bedfec
Gaku52 <Gaku52@example.com>
2022-10-18 18:32:46 +0900

    fix: borderの除去とスマホの並び修正

--- src/components/features/story/Stories/Story.tsx
+++ src/components/features/story/Stories/Story.tsx
@@ -14,14 +14,14 @@ export const Story = ({ title, coverImage, date, excerpt, slug }: Props) => {
   return (
     <Link href={`/posts/${slug}`}>
       <a className="justify-center select-none overflow-hidden w-full h-full vstack sm:flex-row cursor-pointer p-2 focus:outline- ">
-        <div className="m-auto center w-full sm:w-1/3 h-48 sm:h-full bg-neutral-50 sm:bg-transparent">
+        <div className="m-auto center w-auto h-48 sm:w-1/3 sm:h-full bg-neutral-50 sm:bg-transparent">
           <Image
             src={coverImage}
             alt={`Cover Image for ${title}`}
-            className="border-2 w-auto h-full object-cover sm:w-auto sm:h-40"
+            className="w-auto h-full object-cover sm:w-auto sm:h-40"
           />
         </div>
-        <div className="m-auto w-full sm:w-2/3 h-28 sm:h-40 p-3 sm:p-6 vstack gap-2 bg-primary-1">
+        <div className="m-auto w-full sm:w-2/3 h-40 sm:h-40 p-3 sm:p-6 vstack gap-2 bg-primary-1">
           <Date date={date} />
           <h3 className="m-auto sm:text-primary-1 sm:text-xl font-medium">{title}</h3>
           <p className="m-auto text-neutral-700 dark:text-neutral-300 font-normal text-sm line-clamp-2 sm:line-clamp-3">

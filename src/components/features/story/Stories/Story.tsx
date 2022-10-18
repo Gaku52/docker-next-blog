c62351484cdff356320138fa00341e5fa5946a9e
Gaku52 <Gaku52@example.com>
2022-10-18 16:42:14 +0900

    feat: 検証4

--- src/components/features/story/Stories/Story.tsx
+++ src/components/features/story/Stories/Story.tsx
@@ -13,15 +13,15 @@ type Props = {
 export const Story = ({ title, coverImage, date, excerpt, slug }: Props) => {
   return (
     <Link href={`/posts/${slug}`}>
-      <a className="select-none overflow-hidden w-full h-full vstack sm:flex-row cursor-pointer focus:outline-2 ">
-        <div className="center w-full sm:w-1/3 h-52 sm:h-full bg-neutral-50 sm:bg-transparent">
+      <a className="justify-center select-none overflow-hidden w-full h-full vstack sm:flex-row cursor-pointer p-2 focus:outline- ">
+        <div className="m-auto center w-3/4 sm:w-1/3 h-52 sm:h-full bg-neutral-50 sm:bg-transparent">
           <Image
             src={coverImage}
             alt={`Cover Image for ${title}`}
             className="w-full max-w-xs h-full object-cover"
           />
         </div>
-        <div className="sm:w-2/3 p-2 sm:p-6 vstack gap-2 bg-primary-1">
+        <div className="m-auto w-3/4 sm:w-2/3 p-3 sm:p-6 vstack gap-2 bg-primary-1">
           <Date date={date} />
           <h3 className="text-primary-1 text-xl font-medium">{title}</h3>
           <p className="text-neutral-700 dark:text-neutral-300 font-normal text-sm line-clamp-2 sm:line-clamp-3">
--- src/hooks/useBreakPoint.ts
+++ src/hooks/useBreakPoint.ts
@@ -1,12 +1,14 @@
 import { useWindowSize } from './useWindowSize';
 
-type Bp = 'sm' | 'md' | 'lg' | 'xl';
+type Bp = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
 
 const bps = {
+  xs: 576,
   sm: 640,
   md: 768,
   lg: 1024,
   xl: 1280,
+  xxl: 1400,
 };
 
 export const useBreakPoint = (bp: Bp) => {

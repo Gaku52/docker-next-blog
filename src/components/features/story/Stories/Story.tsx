a8180f8ee2c2d47a19205efdbf0c0cd39160d436
Gaku52 <Gaku52@example.com>
2022-10-18 17:40:33 +0900

    feat: レスポンシブ調整

--- src/components/features/story/Stories/Story.tsx
+++ src/components/features/story/Stories/Story.tsx
@@ -14,13 +14,13 @@ export const Story = ({ title, coverImage, date, excerpt, slug }: Props) => {
   return (
     <Link href={`/posts/${slug}`}>
       <a className="justify-center select-none overflow-hidden w-full h-full vstack sm:flex-row cursor-pointer p-2 focus:outline- ">
-        {/* <div className="m-auto center border w-full sm:w-1/3 h-48 sm:h-full bg-neutral-50 sm:bg-transparent">
+        <div className="m-auto center border w-full sm:w-1/3 h-48 sm:h-full bg-neutral-50 sm:bg-transparent">
           <Image
             src={coverImage}
             alt={`Cover Image for ${title}`}
             className="w-full h-full sm:w-auto sm:h-40"
           />
-        </div> */}
+        </div>
         <div className="m-auto w-full sm:w-2/3 h-28 sm:h-40 p-3 sm:p-6 vstack gap-2 bg-primary-1">
           <Date date={date} />
           <h3 className="text-primary-1 text-xl font-medium">{title}</h3>

e6e4216a8326e36bf66c93d8ebd9a95403a1e2e9
Gaku52 <Gaku52@example.com>
2022-11-01 15:35:23 +0900

    feat: Next+Dokcerの環境構築

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // hot reload
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1, //チェック時間
+      aggregateTimeout: 1, // 遅延時間
+      ignored : ['node_modules']
+    };
+    return config;
+  },
   basePath: process.env.GITHUB_ACTIONS ? "/next-blog" : "",
   trailingSlash: true,
 };

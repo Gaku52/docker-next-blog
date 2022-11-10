5c08df5e34aa09b23741d157cc6c7f49fbadc100
Gaku52 <gaku.o.engineer@gmail.com>
2022-11-11 04:08:37 +0900

    update:レスポンシブ対応

--- src/components/features/story/Stories/Stories.tsx
+++ src/components/features/story/Stories/Stories.tsx
@@ -10,11 +10,11 @@ type Props = {
 export const Stories = ({ posts, icon, title }: Props) => {
   return (
     <section>
-      <div className="vstack gap-8">
+      <div className="vstack sm:gap-8 gap-10">
         <h2 className="hstack gap-2 text-primary-1 text-3xl font-bold tracking-tighter leading-tight">
           {icon} {title}
         </h2>
-        <div className="grid sm:grid-cols-1 grid-cols-1 dark:text-white sm:gap-10 gap-8">
+        <div className="grid sm:grid-cols-1 grid-cols-2 dark:text-white sm:gap-10 gap-5">
           {posts.map((post) => (
             <Story
               key={post.slug}

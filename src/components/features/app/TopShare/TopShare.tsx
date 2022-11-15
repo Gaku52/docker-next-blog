5e887a9c083fd941b50a7826f1e20a313638020f
Gaku52 <gaku.o.engineer@gmail.com>
2022-11-15 15:05:33 +0900

    update:タブレットサイズに適応

--- src/components/features/app/TopShare/TopShare.tsx
+++ src/components/features/app/TopShare/TopShare.tsx
@@ -18,7 +18,7 @@ type Props = {
   post: PostType;
 };
 
-const SIZE = 24;
+const SIZE = 20;
 
 export const TopShare: React.VFC<Props> = ({ post }) => {
   const { title, slug
@@ -31,16 +31,16 @@ export const TopShare: React.VFC<Props> = ({ post }) => {
   const tags = post.tags.map((tag) => tag.split(' ')[0]);
 
   return (
-    <div className="select-none vstack gap-3 p-6 bg-primary-2">
+    <div className="select-none vstack gap-3 p-3 bg-primary-2">
       <div className="center gap-2 py-2 px-3 text-lg font-bold text-primary-1 uppercase">
         <MdShare />
         シェアする
       </div>
 
       <div className="w-full center gap-4">
-      <PinterestShareButton {...config} 
+      <PinterestShareButton {...config}
           media={ogImage.url}
-          className=" zoom-box-2 transition-transform"
+          className="zoom-box-2 transition-transform"
         >
           <PinterestIcon size={SIZE} round />
         </PinterestShareButton>
@@ -50,20 +50,20 @@ export const TopShare: React.VFC<Props> = ({ post }) => {
           via={SITE_NAME}
           related={[SITE_NAME, 'Next.js']}
           hashtags={tags}
-          className=" zoom-box-2 transition-transform"
+          className="zoom-box-2 transition-transform"
         >
           <TwitterIcon size={SIZE} round />
         </TwitterShareButton>
         <FacebookShareButton {...config}
-          className=" zoom-box-2 transition-transform">
+          className="zoom-box-2 transition-transform">
           <FacebookIcon size={SIZE} round />
         </FacebookShareButton>
         <LineShareButton {...config}
-          className=" zoom-box-2 transition-transform">
+          className="zoom-box-2 transition-transform">
           <LineIcon size={SIZE} round />
         </LineShareButton>
         <HatenaShareButton {...config}
-          className=" zoom-box-2 transition-transform"
+          className="zoom-box-2 transition-transform"
         >
           <HatenaIcon size={SIZE} round />
         </HatenaShareButton>

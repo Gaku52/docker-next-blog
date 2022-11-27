e8f63fb2099d888f1131a4ec7c9960d31c5bae6b
Gaku52 <gaku.o.engineer@gmail.com>
2022-11-28 01:05:51 +0900

    update:ハンバーガー修正

--- src/components/features/app/Hamburger/Hamburger.tsx
+++ src/components/features/app/Hamburger/Hamburger.tsx
@@ -18,7 +18,7 @@ export const Hamburger: React.VFC<Props> = ({ children }) => {
       className="vstack gap-2 max-h-80 bg-primary-2 shadow-xl border-neutral-300 border-2"
       trigger={
         <button
-          className="icon-btn rounded-full border-2 border-neutral-300 bg-white dark:bg-black shadow-xl"
+          className="icon-btn rounded-full border-2 border-neutral-300 bg-white dark:bg-black shadow-xl outline-none"
           aria-label="hamburger menu"
         >
           <RiMenu4Line size={28} />
@@ -34,10 +34,10 @@ export const Hamburger: React.VFC<Props> = ({ children }) => {
                 new KeyboardEvent('keydown', { key: 'Escape' }),
               )
             }
-            className="text-md text-primary-1 hover:text-teal-800 hover:dark:text-teal-400 focus:text-teal-800 focus:dark:text-teal-400 capitalize select-none cursor-pointer"
+            className="text-primary-1 hover:outline-none hover:text-teal-800 hover:dark:text-teal-400 focus:text-teal-800 focus:dark:text-teal-400 capitalize select-none cursor-pointer"
           >
             <Link href={href} passHref>
-              <a className="hstack gap-3 py-3 px-6">
+              <a className="hstack gap-0 py-3 px-5 hover:underline">
                 {icon} {name}
               </a>
             </Link>

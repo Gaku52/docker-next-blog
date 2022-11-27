733f7d2685bde3f5c6162089d196e57b30018199
Gaku52 <gaku.o.engineer@gmail.com>
2022-11-27 18:11:05 +0900

    update:ヘッダー修正開始11/27

--- src/config/links.tsx
+++ src/config/links.tsx
@@ -1,8 +1,10 @@
 import { AiOutlineInfoCircle } from 'react-icons/ai';
 import { FiTag } from 'react-icons/fi';
+import { RiChatNewLine } from 'react-icons/ri';
 import { LinkType } from '@/types/link';
 
 export const links: LinkType[] = [
+  { name: '記事一覧', href: '/posts/page/1', icon: <RiChatNewLine /> },
   { name: 'タグ', href: '/tags', icon: <FiTag /> },
   { name: '詳細プロフィール', href: '/about', icon: <AiOutlineInfoCircle /> },
 ];

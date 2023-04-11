88fc26042be1a04ceea31667dfa5e29af7723448
Gaku52 <gaku.o.engineer@gmail.com>
2023-04-11 22:17:35 +0900

    :fix: #003 修正削除

--- src/pages/api/webhook.jsx
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    console.log('GitHub webhook triggered');
-    // ここで再デプロイ処理を行うか、他の処理を追加できます。
-    res.status(200).json({ message: 'Webhook received' });
-  } else {
-    res.status(405).json({ message: 'Method not allowed' });
-  }
-}

dc5dd26ca2211dcda70ac0e49334b243fbe1b1ef
Gaku52 <gaku.o.engineer@gmail.com>
2023-04-14 00:51:51 +0900

    update: #263 href修正

--- src/config/sns.tsx
+++ src/config/sns.tsx
@@ -17,9 +17,9 @@ export const sns = [
     icon: <SiTwitter size={20} />,
     label: 'Twitter',
   },
-  // {
-  //   href: process.env.MICRO_CMS_API_BASE_URL,
-  //   icon: <RiAdminFill size={20} />,
-  //   label: 'Admin',
-  // },
+  {
+    href: 'https://4sq33la9ez.microcms.io/apis/blogs',
+    icon: <RiAdminFill size={20} />,
+    label: 'Admin',
+  },
 ];

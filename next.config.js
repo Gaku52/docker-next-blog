6d86dc272ebd4d1e210f9faf374faabf8ded789d
Gaku52 <gaku.o.engineer@gmail.com>
2023-04-15 16:14:23 +0900

    update: #301 next.configの修正

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: process.env.GITHUB_ACTIONS ? "/docker-next-blog" : "",
   trailingSlash: true,
+  esmExternals: true,
+  future: {
+    webpack5: true,
+  },
 };
 
 module.exports = nextConfig;

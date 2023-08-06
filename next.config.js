c2e6876a54c9f0fbba0ad7bfebd95aa6b22ce11a
Gaku <105856239+Gaku52@users.noreply.github.com>
2023-08-06 22:28:36 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
   basePath: process.env.GITHUB_ACTIONS ? "/docker-next-blog" : "",
   trailingSlash: true,
@@ -7,6 +6,18 @@ const nextConfig = {
   future: {
     webpack5: true,
   },
+  async headers() {
+    return [
+      {
+        // APIエンドポイントに対してキャッシュ無効化の設定を追加
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache',
+          },
+        ],
+      },
+    ];
+  },
 };
-
-module.exports = nextConfig;
